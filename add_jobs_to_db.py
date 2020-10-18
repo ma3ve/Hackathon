3cf8210ae97bdd93be70289f0cb86924e6138efc
Prakarsh Bhardwaj <56783108+PrakarshBhardwaj@users.noreply.github.com>
2020-10-19 02:39:47 +0530

    add the 22k init jobs to db

---
+++ add_jobs_to_db.py
@@ -0,0 +1,25 @@
+# Copy paste and run in python manage.py shell
+from main.models import DataModel
+import pandas as pd
+
+df = pd.read_csv('sap_data.csv')
+
+for i in range(df.shape[0]):
+    row = df.iloc[i]
+    if i%200 == 0:
+        print(f"On ---{i}th--- object!")
+    a = DataModel.objects.create(company=row['company'],
+                                    education=row['education'],
+                                    experience= row['experience'],
+                                    industry=row['industry'],
+                                    jobdescription=row['jobdescription'],
+                                    jobid=row['jobid'],
+                                    joblocation_address=row['joblocation_address'],
+                                    jobtitle=row['jobtitle'],
+                                    numberofpositions=row['numberofpositions'],
+                                    payrate=row['payrate'],
+                                    site_name=row['site_name'],
+                                    skills=row['skills'],
+                                    uniq_id=row['uniq_id'],
+                                )
+    a.save()
