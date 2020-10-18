882560dec137834bbac1851ec8688485f0bd7311
emkay <mujammilkhan321@gmail.com>
2020-10-19 03:57:37 +0530

    .

--- add_jobs_to_db.py
+++ add_jobs_to_db.py
@@ -1,25 +1,30 @@
 # Copy paste and run in python manage.py shell
-from main.models import DataModel
+from jobs.models import JobModel
 import pandas as pd
 
-df = pd.read_csv('sap_data.csv')
+df = pd.read_csv('data.csv')
 
 for i in range(df.shape[0]):
     row = df.iloc[i]
-    if i%200 == 0:
-        print(f"On ---{i}th--- object!")
-    a = DataModel.objects.create(company=row['company'],
-                                    education=row['education'],
-                                    experience= row['experience'],
-                                    industry=row['industry'],
-                                    jobdescription=row['jobdescription'],
-                                    jobid=row['jobid'],
-                                    joblocation_address=row['joblocation_address'],
-                                    jobtitle=row['jobtitle'],
-                                    numberofpositions=row['numberofpositions'],
-                                    payrate=row['payrate'],
-                                    site_name=row['site_name'],
-                                    skills=row['skills'],
-                                    uniq_id=row['uniq_id'],
+
+    if (i == 500):
+        break
+
+    x = 0
+    if row['numberofpositions']:
+        x = row['numberofpositions']
+    a = JobModel.objects.create(company=row['company'],
+                                education=row['education'],
+                                experience=row['experience'],
+                                industry=row['industry'],
+                                jobdescription=row['jobdescription'],
+                                jobid=row['jobid'],
+                                joblocation_address=row['joblocation_address'],
+                                jobtitle=row['jobtitle'],
+                                numberofpositions=x,
+                                payrate=row['payrate'],
+                                site_name=row['site_name'],
+                                skills=row['skills'],
+                                uniq_id=row['uniq_id'],
                                 )
     a.save()
