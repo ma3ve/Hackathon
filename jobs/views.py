02b86dc78d718a0176a9b23188e773c5e5c51fc2
Prakarsh Bhardwaj <56783108+PrakarshBhardwaj@users.noreply.github.com>
2020-10-19 14:39:11 +0530

    more filter

--- jobs/views.py
+++ jobs/views.py
@@ -7,12 +7,20 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-def checkloc(req, city_string):
+def checkloc(locs, city_string):
 	city_list = city_string.strip().split(', ')
-	if req in city_string:
-		return True
-	else:
-		return False
+	for loc in locs:
+		if loc in city_string:
+			return True
+	return False
+
+def checkexp(exps, job_exp_cat):
+	job_exp_cat_list = job_exp_cat.strip().split(' ')
+	for exp in exps:
+		if exp in job_exp_cat_list:
+			return True
+	return False
+			
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 20
@@ -24,12 +32,35 @@ class JobModelViewset(viewsets.ModelViewSet):
 	serializer_class = JobModelSerializer
 	queryset = JobModel.objects.all().order_by('-postdate')
 	pagination_class = StandardResultsSetPagination
-	permission_classes = (IsAuthenticated,)
+	# permission_classes = (IsAuthenticated,)
 	
-	@action(detail=True, methods=["GET"])
+	@action(detail=False, methods=["GET"])
 	def location(self, request, *args, **kwargs):
-		loc = kwargs['pk']
+		# loc = kwargs['pk']
+		loc = request.query_params.get('loc').split(',')
 		active_ids = [job.id for job in JobModel.objects.all() if checkloc(loc, job.joblocation_address)]
+		nserv = JobModel.objects.filter(id__in=active_ids).order_by('-postdate')
+		paginator = StandardResultsSetPagination()
+		result_page = paginator.paginate_queryset(nserv, request)
+		serializer = JobModelSerializer(result_page, many=True)
+		return paginator.get_paginated_response(serializer.data)
+	
+	@action(detail=False, methods=["GET"])
+	def skills(self, request, *args, **kwargs):
+		# loc = kwargs['pk']
+		sks = request.query_params.get('skills').split(',')
+		# active_ids = [job.id for job in JobModel.objects.all() if checkloc(loc, job.joblocation_address)]
+		nserv = JobModel.objects.filter(skills__in=sks).order_by('-postdate')
+		paginator = StandardResultsSetPagination()
+		result_page = paginator.paginate_queryset(nserv, request)
+		serializer = JobModelSerializer(result_page, many=True)
+		return paginator.get_paginated_response(serializer.data)
+	
+	@action(detail=False, methods=["GET"])
+	def exp(self, request, *args, **kwargs):
+		# loc = kwargs['pk']
+		exps = request.query_params.get('exp').split(',')
+		active_ids = [job.id for job in JobModel.objects.all() if checkexp(exps, job.job_exp_cat)]
 		nserv = JobModel.objects.filter(id__in=active_ids).order_by('-postdate')
 		paginator = StandardResultsSetPagination()
 		result_page = paginator.paginate_queryset(nserv, request)
