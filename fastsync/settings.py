0e8664af39d74d6cdcf299f12d9c87ab314304cc
Prakarsh Bhardwaj <56783108+PrakarshBhardwaj@users.noreply.github.com>
2020-10-19 10:18:12 +0530

    y

--- fastsync/settings.py
+++ fastsync/settings.py
@@ -34,6 +34,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 30
 }
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
--- jobs/views.py
+++ jobs/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from .models import JobModel
 from .serializers import JobModelSerializer
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 30
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 # Create your views here.
 class JobModelViewset(viewsets.ModelViewSet):
 	serializer_class = JobModelSerializer
 	queryset = JobModel.objects.all().order_by('-postdate')
+	# paginate_by = 10
+	pagination_class = StandardResultsSetPagination
