a981e3ac4f71118f7bd1fc9e1774bd0e6fd3ef22
emkay <mujammilkhan321@gmail.com>
2020-10-19 17:43:18 +0530

    heroku commit

--- fastsync/settings.py
+++ fastsync/settings.py
@@ -178,3 +178,6 @@ DATABASES = {
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['CONN_MAX_AGE'] = 500
+
+DEBUG = False
+ALLOWED_HOSTS = ['fastsync.herokuapp.com']
